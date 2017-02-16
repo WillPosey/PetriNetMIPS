7f6e82144a11ffb2ba2a938487bd94d2541331b4
WillPosey <navarreposey11@gmail.com>
2017-02-16 14:53:34 -0500

    UHHHHHHHH YEP

--- MIPSsim.cpp
+++ MIPSsim.cpp
@@ -1,12 +1,138 @@
-#include <pthread.h>
+#define DEBUG
+
+#define ALL_COMPLETE 2
+
+#include <thread>
+#include <chrono>
 #include <string>
-#include <condition_variable>
 #include <mutex>
+#include <iostream>
 
 using namespace std;
 
-condition_variable clockSignal;
-mutex clockLock;
+typedef void* (*thread_func_ptr)(void*);
+
+int numCycles = 5;
+
+/*****************************************/
+bool clockEdge = false;
+mutex clockMutex;
+
+int countClockedComplete = 0;
+bool clockedComplete = false;
+mutex clockedCompleteMutex;
+
+int countCycleComplete = 0;
+bool cycleComplete = false;
+mutex cycleMutex;
+
+bool executionComplete = false;
+mutex completeMutex;
+
+/*****************************************/
+mutex outputLock;
+
+void dbg(string output)
+{
+    #ifdef DEBUG
+        outputLock.lock();
+        cout << output << endl;
+        flush(cout);
+        outputLock.unlock();
+    #endif
+}
+
+/*****************************************/
+bool CheckClockRisingEdge()
+{
+    bool risingEdge;
+    clockMutex.lock();
+    risingEdge = clockEdge;
+    clockMutex.unlock();
+    return risingEdge;
+}
+
+/*****************************************/
+bool CheckClockFallingEdge()
+{
+    bool fallingEdge;
+    clockMutex.lock();
+    fallingEdge = !clockEdge;
+    clockMutex.unlock();
+    return fallingEdge;
+}
+
+/*****************************************/
+void SetClockEdge(bool risingEdge)
+{
+    clockMutex.lock();
+    clockEdge = risingEdge;
+    clockMutex.unlock();
+}
+
+/*****************************************/
+void SetClocked()
+{
+    clockedCompleteMutex.lock();
+    countClockedComplete++;
+    if(countClockedComplete == ALL_COMPLETE)
+    {
+        countClockedComplete = 0;
+        clockedComplete = true;
+    }
+    clockedCompleteMutex.unlock();
+}
+
+/*****************************************/
+bool CheckClocked()
+{
+    bool clocked;
+    clockedCompleteMutex.lock();
+    clocked = clockedComplete;
+    clockedCompleteMutex.unlock();
+    return clocked;
+}
+
+/*****************************************/
+void SetCycleComplete()
+{
+    completeMutex.lock();
+    countCycleComplete++;
+    if(countCycleComplete == ALL_COMPLETE)
+    {
+        countCycleComplete = 0;
+        cycleComplete = true;
+    }
+    completeMutex.unlock();
+}
+
+/*****************************************/
+bool CheckCycleComplete()
+{
+    bool complete;
+    completeMutex.lock();
+    complete = cycleComplete;
+    completeMutex.unlock();
+    return complete;
+}
+
+/*****************************************/
+void SetExecutionComplete()
+{
+    completeMutex.lock();
+    executionComplete = true;
+    completeMutex.unlock();
+}
+
+/*****************************************/
+bool CheckExecutionComplete()
+{
+    bool complete;
+    completeMutex.lock();
+    complete = executionComplete;
+    completeMutex.unlock();
+    return complete;
+}
 
 /**************************/
 struct InstructionString
@@ -15,6 +141,14 @@ struct InstructionString
     string destReg;
     string srcReg1;
     string srcReg2;
+
+    string ToString()
+    {
+        return "<" + opcode + ","
+                   + destReg + ","
+                   + srcReg1 + ","
+                   + srcReg2 + ">";
+    }
 };
 
 /**************************/
@@ -24,6 +158,14 @@ struct InstructionDecode
     int destRegNum;
     int srcReg1Num;
     int srcReg2Num;
+
+    string ToString()
+    {
+        return "<" + opcode + ","
+                   + to_string(destRegNum) + ","
+                   + to_string(srcReg1Num) + ","
+                   + to_string(srcReg2Num) + ">";
+    }
 };
 
 /**************************/
@@ -31,6 +173,12 @@ struct MemoryValue
 {
     int location;
     int value;
+
+    string ToString()
+    {
+        return "<" + to_string(location) + ","
+                   + to_string(value) + ">";
+    }
 };
 
 /**************************/
@@ -38,10 +186,17 @@ struct RegisterValue
 {
     int regNum;
     int value;
+
+    string ToString()
+    {
+        return "<R" + to_string(regNum) + ","
+                   + to_string(value) + ">";
+    }
 };
 
 /******************************************************************************************/
-class CycleThread {
+class CycleThread
+{
 public:
 	CycleThread(void* (*f)(void*), void* obj)
 	{
@@ -67,104 +222,355 @@ private:
 };
 
 /******************************************************************************************/
-template <class I, class O>
-class Buffer {
+template <class In, class Out>
+class Buffer
+{
 public:
+    Buffer(thread_func_ptr thrPtr) : thread_func(thrPtr){ isEmpty = true; Initialize(); StartThread();};
     /******************************************/
     bool Empty()
     {
         if(isEmpty)
         {
-            unique_lock<mutex> lock(readLock);
-            hasRead.notify_one();
+            readLock.lock();
+            hasRead = true;
+            readLock.unlock();
         }
         return isEmpty;
     }
     /******************************************/
     void NoWrite()
     {
-        unique_lock<mutex> lock(writeLock);
+        writeLock.lock();
         noWrite = true;
-        hasWritten.notify_one();
+        hasWritten = true;
+        writeLock.unlock();
     }
     /******************************************/
-    void Write(I writeI)
+    void Write(In writeInput)
     {
-        unique_lock<mutex> lock(writeLock);
+        writeLock.lock();
         noWrite = false;
-        tempInput = writeI;
-        hasWritten.notify_one();
+        hasWritten = true;
+        tempInput = writeInput;
+        writeLock.unlock();
     }
     /******************************************/
-    O Read()
+    Out Read()
     {
-        O outputData;
-        unique_lock<mutex> lock(readLock);
+        Out outputData;
+        readLock.lock();
         outputData = ReadData();
-        hasRead.notify_one();
+        hasRead = true;
+        readLock.unlock();
         return outputData;
+    }
+    /******************************************/
+    void Join()
+    {
+        thread->JoinThread();
     }
     /******************************************/
     virtual string GetContent() = 0;
 
 protected:
     CycleThread* thread;
-    I tempInput;
+    thread_func_ptr thread_func;
+    In tempInput;
     bool isEmpty;
     bool noWrite;
 
     mutex writeLock, readLock;
-    condition_variable hasRead, hasWritten;
+    bool hasRead, hasWritten;
 
-    virtual void Initialize() = 0;
-    virtual void StartThread() = 0;
-    virtual O ReadData() = 0;
-    virtual O TestReadData() = 0;
+    bool ReadComplete()
+    {
+        bool readCmplt;
+        readLock.lock();
+        readCmplt = hasRead;
+        readLock.unlock();
+        return readCmplt;
+    }
+
+    bool WriteComplete()
+    {
+        bool writeCmplt;
+        writeLock.lock();
+        writeCmplt = hasWritten;
+        writeLock.unlock();
+        return writeCmplt;
+    }
+
+    virtual void Initialize(){};
+    virtual void StartThread(){thread = new CycleThread(thread_func, (void*) this);};
+    virtual Out ReadData() = 0;
+    virtual Out TestRead() = 0;
     virtual void WriteData() = 0;
 };
 
 /******************************************************************************************/
-void* thread_LIB(void* object)
-{
-
-}
-
-class LoadInstrBuffer : protected Buffer<InstructionString, int>
+void* thread_LIB(void* object);
+class LoadInstrBuffer : public Buffer<InstructionDecode, InstructionDecode>
 {
 public:
-    friend void* thread_LIB(void*);
-    LoadInstrBuffer() : Buffer(){Initialize(); StartThread();}
-    string GetContent() {return "hi";}
+    friend void* thread_LIB(void* object);
+    LoadInstrBuffer() : Buffer(thread_LIB){}
+
+    string GetContent()
+    {
+        if(!isEmpty)
+            return "LIB:" + currentInstr.ToString();
+        else
+            return "LIB:";
+    }
+
 private:
-    /******************************************/
-    void Initialize()
-    {
+    InstructionDecode currentInstr;
 
-    }
     /******************************************/
-    void StartThread()
-    {
+    InstructionDecode ReadData() {return currentInstr;}
 
-    }
     /******************************************/
-    int ReadData()
-    {
+    void WriteData() {currentInstr = tempInput;}
 
-    }
     /******************************************/
-    void WriteData()
-    {
-
-    }
-    /******************************************/
-    int TestReadData()
-    {
-
-    }
+    InstructionDecode TestRead() {return ReadData();}
 };
 
+void* thread_LIB(void* object)
+{
+    LoadInstrBuffer* LIB = (LoadInstrBuffer*) object;
+
+    while(1)
+    {
+        while(!CheckClockRisingEdge())
+            this_thread::sleep_for(chrono::milliseconds(5));
+        SetClocked();
+
+dbg(LIB->GetContent());
+
+        while(!CheckClockFallingEdge())
+            this_thread::sleep_for(chrono::milliseconds(5));
+        SetCycleComplete();
+        if(CheckExecutionComplete())
+            break;
+    }
+}
+
 /******************************************************************************************/
-class Transition {
+void* thread_AIB(void* object);
+class ArithInstrBuffer : public Buffer<InstructionDecode, InstructionDecode>
+{
+public:
+    friend void* thread_AIB(void* object);
+    ArithInstrBuffer() : Buffer(thread_AIB){}
+
+    string GetContent()
+    {
+        if(!isEmpty)
+            return "AIB:" + currentInstr.ToString();
+        else
+            return "AIB:";
+    }
+
+private:
+    InstructionDecode currentInstr;
+
+    /******************************************/
+    InstructionDecode ReadData() {return currentInstr;}
+
+    /******************************************/
+    void WriteData() {currentInstr = tempInput;}
+
+    /******************************************/
+    InstructionDecode TestRead() {return ReadData();}
+};
+
+void* thread_AIB(void* object)
+{
+    ArithInstrBuffer* AIB = (ArithInstrBuffer*) object;
+
+    while(1)
+    {
+        while(!CheckClockRisingEdge())
+            this_thread::sleep_for(chrono::milliseconds(5));
+        SetClocked();
+
+dbg(AIB->GetContent());
+
+        while(!CheckClockFallingEdge())
+            this_thread::sleep_for(chrono::milliseconds(5));
+        SetCycleComplete();
+        if(CheckExecutionComplete())
+            break;
+    }
+}
+
+/******************************************************************************************/
+void* thread_INB(void* object);
+class InstructionBuffer : public Buffer<InstructionDecode, InstructionDecode>
+{
+public:
+    friend void* thread_INB(void* object);
+    InstructionBuffer() : Buffer(thread_INB){}
+
+    string GetContent()
+    {
+        string content = "INB:";
+        if(!isEmpty)
+        {
+            for(int i=head; i<numInstr; i++)
+            {
+                content += instructions[i].ToString();
+                if(i!=(numInstr-1))
+                    content += ",";
+            }
+        }
+        return content;
+    }
+
+private:
+    InstructionDecode instructions[16];
+    int head;
+    int numInstr;
+
+    void Initialize()
+    {
+        head = numInstr = 0;
+    }
+
+    /******************************************/
+    InstructionDecode ReadData() {return instructions[head];}
+
+    /******************************************/
+    void WriteData() {instructions[head+numInstr] = tempInput;}
+
+    /******************************************/
+    InstructionDecode TestRead() {return ReadData();}
+};
+
+void* thread_INB(void* object)
+{
+    InstructionBuffer* INB = (InstructionBuffer*) object;
+
+    while(1)
+    {
+        while(!CheckClockRisingEdge())
+            this_thread::sleep_for(chrono::milliseconds(5));
+        SetClocked();
+
+dbg(INB->GetContent());
+
+        while(!CheckClockFallingEdge())
+            this_thread::sleep_for(chrono::milliseconds(5));
+        SetCycleComplete();
+        if(CheckExecutionComplete())
+            break;
+    }
+}
+
+/******************************************************************************************/
+void* thread_ADB(void* object);
+class AddressBuffer : public Buffer<RegisterValue, RegisterValue>
+{
+public:
+    friend void* thread_ADB(void* object);
+    AddressBuffer() : Buffer(thread_ADB){}
+
+    string GetContent()
+    {
+        if(!isEmpty)
+            return "ADB:" + currentAddress.ToString();
+        else
+            return "ADB:";
+    }
+
+private:
+    RegisterValue currentAddress;
+
+    /******************************************/
+    RegisterValue ReadData() {return currentAddress;}
+
+    /******************************************/
+    void WriteData() {currentAddress = tempInput;}
+
+    /******************************************/
+    RegisterValue TestRead() {return ReadData();}
+};
+
+void* thread_ADB(void* object)
+{
+    AddressBuffer* ADB = (AddressBuffer*) object;
+
+    while(1)
+    {
+        while(!CheckClockRisingEdge())
+            this_thread::sleep_for(chrono::milliseconds(5));
+        SetClocked();
+
+dbg(ADB->GetContent());
+
+        while(!CheckClockFallingEdge())
+            this_thread::sleep_for(chrono::milliseconds(5));
+        SetCycleComplete();
+        if(CheckExecutionComplete())
+            break;
+    }
+}
+
+/******************************************************************************************/
+void* thread_DAM(void* object);
+class DataMemory : public Buffer<MemoryValue, MemoryValue>
+{
+public:
+    friend void* thread_DAM(void* object);
+    DataMemory() : Buffer(thread_DAM){}
+
+    string GetContent()
+    {
+        if(!isEmpty)
+            return "DAM:" + currentAddress.ToString();
+        else
+            return "DAM:";
+    }
+
+private:
+    MemoryValue dataMem[8];
+
+    /******************************************/
+    RegisterValue ReadData() {return currentAddress;}
+
+    /******************************************/
+    void WriteData() {currentAddress = tempInput;}
+
+    /******************************************/
+    RegisterValue TestRead() {return ReadData();}
+};
+
+/******************************************/
+void* thread_DAM(void* object)
+{
+    DataMemory* DAM = (DataMemory*) object;
+
+    while(1)
+    {
+        while(!CheckClockRisingEdge())
+            this_thread::sleep_for(chrono::milliseconds(5));
+        SetClocked();
+
+dbg(DAM->GetContent());
+
+        while(!CheckClockFallingEdge())
+            this_thread::sleep_for(chrono::milliseconds(5));
+        SetCycleComplete();
+        if(CheckExecutionComplete())
+            break;
+    }
+}
+
+/******************************************************************************************/
+class Transition
+{
 public:
 
 protected:
@@ -176,6 +582,41 @@ protected:
 /******************************************************************************************/
 int main()
 {
-    LoadInstrBuffer buf();
+    LoadInstrBuffer LIB;
+    ArithInstrBuffer AIB;
+    InstructionBuffer INB;
+    AddressBuffer ADB;
+
+    for(int i=0; i<5; i++)
+    {
+        // Check if last cycle, if so notify threads to exit
+        if(i==(numCycles-1))
+            SetExecutionComplete();
+
+        // Clock each thread with rising edge
+        SetClockEdge(true);
+
+        // Wait for clock to be registered
+        while(!CheckClocked())
+            this_thread::sleep_for(chrono::milliseconds(5));
+
+        // Clock falling edge
+        SetClockEdge(false);
+
+        // Wait for each thread to complete its loop and register the falling clock edge
+        while(!CheckCycleComplete())
+            this_thread::sleep_for(chrono::milliseconds(5));
+
+        // Reset globals
+        cycleComplete = false;
+        clockedComplete = false;
+    }
+
+    // Join all threads
+    LIB.Join();
+    AIB.Join();
+    INB.Join();
+    ADB.Join();
+
     return 0;
 }
